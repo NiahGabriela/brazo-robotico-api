8d6a9e1cdc9ed0ed16fdee0abd1e0ac1e2cbddc6
NiahGabriela <dghg.12@hotmail.com>
2018-05-26 21:33:53 -0500

    agregando los cambios del user

--- models/users.js
+++ models/users.js
@@ -17,15 +17,16 @@ exports.create = function(username, email, password) { //acomodar bien la funcio
   age=null; gender=null; active=null; permisosChidos=false;
   var response = db.INSERT(`User`, //nombre de la base de datos
   `name,age,gender,email,active,permisosChidos`, // columnas de la base de la tabla
-  `\'${username}\',\'${age}\',\'${gender}\',\'${email}\',\'${active}\',\'${password}\'`, //valores para rellenar
+  `\'${username}\',\'${age}\',\'${gender}\',\'${email}\',\'${active}\',\'${permisosChidos}\',\'${password}\'`, //valores para rellenar
   null,
   `*`);
   return response;
   };
 
-  exports.listAll = funcion(){
+exports.listAll = funcion(){
 
-  }
+
+}
 
 //se ecarga del modelo del negocio y mandar
 //todo a la base de datos
