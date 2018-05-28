2f249d6d212d28438cc00b7811efa9051a74911f
NiahGabriela <dghg.12@hotmail.com>
2018-05-28 12:08:49 -0500

    cambie los puertos

--- app.js
+++ app.js
@@ -4,4 +4,4 @@ const routes = require('./routes');
 
 app.use('/', routes);
 
-app.listen(3000, () => console.log('Arm is alive on port 3000!'));
+app.listen(process.env.PORT || 3000, () => console.log('Arm is alive on port 3000!'));
