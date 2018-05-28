72a883d7882b2525bb31f89f511e5325316b6263
pilaralejandra <pilarale95@gmail.com>
2018-05-27 19:18:38 -0500

    Select funcionando :D

---
+++ controllers/games.js
@@ -0,0 +1,16 @@
+const {Router} = require ('express');
+const gameController = Router();
+const model = require ('../models/games');
+
+gameController.create = function(req, res) {
+  var response = model.create(req.body.gameLogId,req.myTime);
+  return response;
+};
+
+gameController.SelectGames = (req, res, callback) => {
+  model.SelectGames(req, res, (err, data) => {
+  callback(err, data);
+  });
+};
+
+module.exports = gameController;
